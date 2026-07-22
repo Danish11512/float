278adeb097a090d006139ca3221a4560f6183b37
Danish11512 <danish.faruqi1@gmail.com>
2026-07-22 15:25:49 -0400

    restart

--- Sources/Models/Model.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-// TODO: Define domain models as Sendable structs
-// Example:
-// struct FloatItem: Sendable, Identifiable, Codable {
-//     let id: UUID
-//     var title: String
-//     var updatedAt: Date
-// }
